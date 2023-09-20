2aae4785afbac37068068e97057a5d3a5211cc89
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-09-20 15:48:51 -0300

    Alterando imagens

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const html = document.querySelector('html'); // referenciat tag html
-const focoBt = document.querySelector('.app__card-button--foco');
+const html = document.querySelector('html'); // referencia tag html
+const focoBt = document.querySelector('.app__card-button--foco'); // referencia tag button pelo nome da classe
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image'); // referencia tag img pela classe
 
 focoBt.addEventListener('click', () => { // passando uma função para quando ocorrer um click no botão
-    html.setAttribute('data-contexto', 'foco')
+    html.setAttribute('data-contexto', 'foco'); // mudando o valor do data contexto, cor de fundo, para descanso curto -> está no css
+    banner.setAttribute('src', '/imagens/foco.png') // alterando imagem quando clico em foco
 })
 
 curtoBt.addEventListener('click', () => { 
-    html.setAttribute('data-contexto', 'descanso-curto') // mudando o valor do data contexto para descanso curto -> está no css
+    html.setAttribute('data-contexto', 'descanso-curto'); 
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBt.addEventListener('click', () => { 
-    html.setAttribute('data-contexto', 'descanso-longo')
+    html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
