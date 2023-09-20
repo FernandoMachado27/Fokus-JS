c5935010690889fc14b093a69da65b73701d751d
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-09-20 15:56:38 -0300

    Separando funções e métodos

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image'); // referencia tag img pela classe
 
 focoBt.addEventListener('click', () => { // passando uma função para quando ocorrer um click no botão
-    html.setAttribute('data-contexto', 'foco'); // mudando o valor do data contexto, cor de fundo, para descanso curto -> está no css
-    banner.setAttribute('src', '/imagens/foco.png') // alterando imagem quando clico em foco
+    alterarContexto('foco')
 })
 
 curtoBt.addEventListener('click', () => { 
-    html.setAttribute('data-contexto', 'descanso-curto'); 
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
 })
 
 longoBt.addEventListener('click', () => { 
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 })
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto); // mudando o valor do data contexto, cor de fundo -> está no css
+    banner.setAttribute('src', `/imagens/${contexto}.png`); // template string, inserindo junto de um elemento html, alterando a imagem
+}
