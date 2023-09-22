27e6d11cf402c98aca0c022090e9cf031327bd67
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-09-22 13:35:39 -0300

    Iniciando o temporizador (setInterval)

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image'); // referencia tag img pela classe
 const titulo = document.querySelector('.app__title'); // referencia da tag h1 pelo nome da classe
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause'); //buscando por id
+
 const musicaFocoinput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 musicaFocoinput.addEventListener('change', () => { // evento para trabalhar com input do tipo checkbox
@@ -58,4 +64,16 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => { // função contagem
+    //iniciar();
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', contagemRegressiva); // quandop tiver o evento de click chame a função contagem regressiva
+
+function iniciar() {
+    intervaloId = setInterval(contagemRegressiva, 1000); // executa o metodo dentro deste periodo de tempo
 }
